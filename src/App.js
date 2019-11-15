31fe2f94f5f7f2e367b3efb69c93086a7fd711a4
Sofia Rodriguez <sofiaromorales@gmail.com>
2019-11-15 16:18:45 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+import './App.css';
+
+class App extends React.Component {
+
+    componentDidMount() {
+        window.location.replace("http://setlife.network");
+    }
+
+    render() {
+
+
+
+        return (
+            <div className='App'>
+                url fix
+            </div>
+        )
+    }
+}
+
+export default App
